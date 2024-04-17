92710a43d27494f4d87a3ff000bc76918db9af7b
kruthikakolume <kolume.k@northeastern.edu>
2024-04-17 18:48:02 -0400

    Version 1

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=8000;
+export const MONGO_URI="mongodb+srv://kkruthika:neuwebdeskk@cluster0.ky8qozg.mongodb.net/test";
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, MONGO_URI } from "./config.js";
+import { router as bookRouter } from "./routes/book.js";
+import cors from "cors";
+
+const app = express();
+// app.use(cors());
+
+// app.use(cors({
+//   origin: 'http://localhost:5173' // Only allow this origin to access the resources
+// }));
+
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Access-Control-Allow-Origin','Content-Type'],
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+/**app.use(
+     cors({
+         origin: 'http://localhost:3000',
+         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+         allowedHeaders: ['Content-Type']
+     })
+)**/
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(234).send("Welcome to MERN stack tutorial");
+});
+
+app.use("/api/v1/books", bookRouter);
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to the database sucessfully");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
